ffcb976da17a116ba3f85a7a657446541a78a69c
Ishan Goyal <ishang974@gmail.com>
2026-01-26 17:17:11 +0530

    New Updates

--- react-redux-demo/src/App.jsx
+++ react-redux-demo/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import { Provider } from 'react-redux'
 import store from './redux/store'
 import CakeContainer from './components/CakeContainer'
+import HooksCakeContainer from './components/HooksCakeContainer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
   return (
     <>
     <Provider store={store}>
+      <HooksCakeContainer />
       <CakeContainer/>
     </Provider>
     </>
