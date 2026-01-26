f38aff12d6954a29c747c77ef6924a8f48db935c
Ishan Goyal <ishang974@gmail.com>
2026-01-26 19:18:36 +0530

    Logger

--- react-redux-demo/src/redux/store.jsx
+++ react-redux-demo/src/redux/store.jsx
@@ -1,6 +1,7 @@
-import {combineReducers, createStore} from 'redux';
+import {combineReducers, createStore,applyMiddleware} from 'redux';
 import cakeReducer from './cakes/cakeReducer';
 import rootReducer from './rootReducer';
+import logger from 'redux-logger';
 // const store = createStore(cakeReducer);
-const store = createStore(rootReducer)
+const store = createStore(rootReducer,applyMiddleware(logger));
 export default store;
