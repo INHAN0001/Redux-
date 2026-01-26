39731567e4040f51f4b5701ae60209c001561f66
Ishan Goyal <ishang974@gmail.com>
2026-01-26 14:04:11 +0530

    New Updates

--- react-redux-demo/src/App.jsx
+++ react-redux-demo/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 import CakeContainer from './components/CakeContainer'
 
 function App() {
@@ -9,7 +11,9 @@ function App() {
 
   return (
     <>
+    <Provider store={store}>
       <CakeContainer/>
+    </Provider>
     </>
   )
 }
